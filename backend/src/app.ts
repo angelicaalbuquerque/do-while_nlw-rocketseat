e77f86d8a90b17bf2eb4186b5b846bad2fc8a585
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-19 19:07:10 -0300

    Criação de rota de login e callback do Github

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,18 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
+
+app.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+  );
+});
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
 
 app.listen(4000, () => console.log(`🚀 Server is running on PORT 4000`));
