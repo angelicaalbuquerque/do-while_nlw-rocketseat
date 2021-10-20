ff9fe29b92aa0562f80cb638861f54d66b7a16a6
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-19 22:49:59 -0300

    Verificação de usuário no DB

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import prismaClient from "../prisma";
 
 interface IAccessTokenResponse {
   access_token: string;
@@ -35,6 +36,25 @@ class AuthenticateUserService {
         },
       },
     );
+
+    const { login, id, avatar_url, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    });
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
 
     return response.data;
   }
