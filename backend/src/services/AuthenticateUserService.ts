69eaeb47da5770b80debe20e8c470ea45e9454d6
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-19 23:07:26 -0300

    Criação do token / autenticação do usuário

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import prismaClient from "../prisma";
+import { sign } from "jsonwebtoken";
 
 interface IAccessTokenResponse {
   access_token: string;
@@ -56,7 +57,23 @@ class AuthenticateUserService {
       });
     }
 
-    return response.data;
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      // secret para validar o token
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      },
+    );
+
+    return { token, user };
   }
 }
 
