f6d2fe0d5510162b21a78b8fe8e6e7e40f2c7f0b
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-19 21:31:29 -0300

    Recuperar access_token e infos do user no github

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import "dotenv/config";
+import { router } from "./routes";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 app.get("/github", (request, response) => {
   response.redirect(
