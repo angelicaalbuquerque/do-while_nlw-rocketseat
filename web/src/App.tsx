0a6417a98643c8646d6f74fd49bccc76bf25bd7b
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-22 14:18:23 -0300

    Funcionalidade signOut

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,17 @@
+import { useContext } from "react";
 import styles from "./App.module.scss";
 import { LoginBox } from "./components/LoginBox";
 import { MessageList } from "./components/MessageList";
+import { SendMessageForm } from "./components/SendMessageForm";
+import { AuthContext } from "./contexts/auth";
 
 export function App() {
+  const { user } = useContext(AuthContext);
+
   return (
     <main className={styles.contentWrapper}>
       <MessageList />
-      <LoginBox />
+      {!!user ? <SendMessageForm /> : <LoginBox />}
     </main>
   );
 }
---
+++ web/src/components/SendMessageForm/index.tsx
@@ -0,0 +1,38 @@
+import { useContext } from "react";
+import { VscGithubInverted, VscSignOut } from "react-icons/vsc";
+import { AuthContext } from "../../contexts/auth";
+import styles from "./styles.module.scss";
+
+export function SendMessageForm() {
+  const { user, signOut } = useContext(AuthContext);
+
+  return (
+    <div className={styles.sendMessageFormWrapper}>
+      <button onClick={signOut} className={styles.signOutButton}>
+        <VscSignOut size="32" />
+      </button>
+
+      <header className={styles.userInformation}>
+        <div className={styles.userImage}>
+          <img src={user?.avatar_url} alt={user?.name} />
+        </div>
+        <strong className={styles.userName}>{user?.name}</strong>
+        <span className={styles.userGithub}>
+          <VscGithubInverted size="16" />
+          {user?.login}
+        </span>
+      </header>
+
+      <form className={styles.sendMessageForm}>
+        <label htmlFor="message">Mensagem</label>
+        <textarea
+          name="message"
+          id="message"
+          placeholder="Qual é a sua expectativa para o evento?"
+        />
+
+        <button type="submit">Enviar mensagem</button>
+      </form>
+    </div>
+  );
+}
