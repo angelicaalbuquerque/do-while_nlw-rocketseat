aa54e6092ca8b3f732f7b6d26f4edc76305506f2
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-22 12:46:15 -0300

    Login com Github com códigio na URL

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,12 +1,26 @@
+import { useEffect } from "react";
 import { VscGithubInverted } from "react-icons/vsc";
 
 import styles from "./styles.module.scss";
 
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=e144fdb9ec2f062609c4`;
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes("?code=");
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split("?code=");
+
+      console.log({ urlWithoutCode, githubCode });
+    }
+  }, []);
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="$" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
         Entrar com Github
       </a>
