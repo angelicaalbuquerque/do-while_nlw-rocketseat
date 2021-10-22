e3576009503e2d38dfe51a4091af474970a7daf0
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-22 13:02:44 -0300

    Fluxo semipronto de autenticação

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,10 +1,34 @@
 import { useEffect } from "react";
 import { VscGithubInverted } from "react-icons/vsc";
+import { api } from "../../services/api";
 
 import styles from "./styles.module.scss";
 
+//Retorno da chamada para a API
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  };
+};
+
 export function LoginBox() {
   const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=e144fdb9ec2f062609c4`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>("authenticate", {
+      code: githubCode,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem("@dowhile:token", token);
+
+    console.log(user);
+  }
 
   useEffect(() => {
     const url = window.location.href;
@@ -14,6 +38,8 @@ export function LoginBox() {
       const [urlWithoutCode, githubCode] = url.split("?code=");
 
       window.history.pushState({}, "", urlWithoutCode);
+
+      signIn(githubCode);
     }
   }, []);
 
