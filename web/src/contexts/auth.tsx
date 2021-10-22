189ea0a84db79bd6e11c2a7961f0537493313c5c
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-22 13:42:36 -0300

    Finalização de contexto

--- web/src/contexts/auth.tsx
+++ web/src/contexts/auth.tsx
@@ -11,6 +11,7 @@ type User = {
 type AuthContextData = {
   user: User | null;
   signInUrl: string;
+  signOut: () => void;
 };
 
 export const AuthContext = createContext({} as AuthContextData);
@@ -47,6 +48,11 @@ export function AuthProvider(props: AuthProvider) {
     setUser(user);
   }
 
+  function signOut() {
+    setUser(null);
+    localStorage.removeItem("@dowhile:token");
+  }
+
   useEffect(() => {
     const token = localStorage.getItem("@dowhile:token");
 
@@ -73,7 +79,7 @@ export function AuthProvider(props: AuthProvider) {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ signInUrl, user }}>
+    <AuthContext.Provider value={{ signInUrl, user, signOut }}>
       {props.children}
     </AuthContext.Provider>
   );
