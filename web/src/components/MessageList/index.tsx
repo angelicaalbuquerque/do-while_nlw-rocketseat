92c620fea0b27797505fc10bd047b418e6aeb471
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-21 23:26:22 -0300

    Estilização do componente MessageList

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,3 +1,61 @@
+import styles from "./style.module.scss";
+
+import logoImg from "../../assets/logo.svg";
+
 export function MessageList() {
-  return <h1>MessageList</h1>;
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/angelicaalbuquerque.png"
+                alt="Angélica Albuquerque"
+              />
+            </div>
+            <span>Angélica Albuquerque</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/angelicaalbuquerque.png"
+                alt="Angélica Albuquerque"
+              />
+            </div>
+            <span>Angélica Albuquerque</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/angelicaalbuquerque.png"
+                alt="Angélica Albuquerque"
+              />
+            </div>
+            <span>Angélica Albuquerque</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
