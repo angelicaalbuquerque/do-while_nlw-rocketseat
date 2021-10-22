335cc841f588d5e1114aafbd81f10f4d8ef81d34
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-21 22:58:47 -0300

    Estilização LoginBox

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.scss";
 
 export function LoginBox() {
   return (
-    <div className={styles.LoginBoxWrapper}>
+    <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
       <a href="$" className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
