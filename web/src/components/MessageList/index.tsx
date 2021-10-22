12c67469c8c982c1ee04e1d4f745d582fd685cc4
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-22 15:55:01 -0300

    Finalização da lógica de mensagens

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -4,6 +4,7 @@ import logoImg from "../../assets/logo.svg";
 
 import { api } from "../../services/api";
 import { useEffect, useState } from "react";
+import io from "socket.io-client";
 
 type Message = {
   id: string;
@@ -14,11 +15,31 @@ type Message = {
   };
 };
 
+const messagesQueue: Message[] = [];
+
+const socket = io("http://localhost:4000");
+
+socket.on("new_message", (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
 
   useEffect(() => {
-    api.get<Message[]>("messages/Last3").then((response) => {
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages((prevState) =>
+          [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean),
+        );
+
+        messagesQueue.shift();
+      }
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    api.get<Message[]>("messages/last3").then((response) => {
       setMessages(response.data);
     });
   }, []);
