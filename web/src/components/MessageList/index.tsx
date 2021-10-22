a9c87aca4b5af7c18617407c82b16acfbf459985
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-22 12:32:55 -0300

    componente MessageList mostrando mgs do backend

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -2,59 +2,45 @@ import styles from "./style.module.scss";
 
 import logoImg from "../../assets/logo.svg";
 
+import { api } from "../../services/api";
+import { useEffect, useState } from "react";
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
+
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>("messages/Last3").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/angelicaalbuquerque.png"
-                alt="Angélica Albuquerque"
-              />
-            </div>
-            <span>Angélica Albuquerque</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/angelicaalbuquerque.png"
-                alt="Angélica Albuquerque"
-              />
-            </div>
-            <span>Angélica Albuquerque</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/angelicaalbuquerque.png"
-                alt="Angélica Albuquerque"
-              />
-            </div>
-            <span>Angélica Albuquerque</span>
-          </div>
-        </li>
+        {messages.map((message) => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
