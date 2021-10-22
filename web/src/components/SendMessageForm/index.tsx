b8aa4abe81e3066b7d061faba57d8e1b3dc70221
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-22 14:28:12 -0300

    Cadastro de mensagem e limpeza de textarea

--- web/src/components/SendMessageForm/index.tsx
+++ web/src/components/SendMessageForm/index.tsx
@@ -1,10 +1,24 @@
-import { useContext } from "react";
+import { useContext, useState, FormEvent } from "react";
 import { VscGithubInverted, VscSignOut } from "react-icons/vsc";
 import { AuthContext } from "../../contexts/auth";
+import { api } from "../../services/api";
 import styles from "./styles.module.scss";
 
 export function SendMessageForm() {
   const { user, signOut } = useContext(AuthContext);
+  const [message, setMessage] = useState("");
+
+  async function handleSendMessage(event: FormEvent) {
+    event.preventDefault();
+
+    if (!message.trim()) {
+      return;
+    }
+
+    await api.post("messages", { message });
+
+    setMessage("");
+  }
 
   return (
     <div className={styles.sendMessageFormWrapper}>
@@ -23,12 +37,14 @@ export function SendMessageForm() {
         </span>
       </header>
 
-      <form className={styles.sendMessageForm}>
+      <form onSubmit={handleSendMessage} className={styles.sendMessageForm}>
         <label htmlFor="message">Mensagem</label>
         <textarea
           name="message"
           id="message"
           placeholder="Qual é a sua expectativa para o evento?"
+          onChange={(event) => setMessage(event.target.value)}
+          value={message}
         />
 
         <button type="submit">Enviar mensagem</button>
--- web/src/contexts/auth.tsx
+++ web/src/contexts/auth.tsx
@@ -45,6 +45,8 @@ export function AuthProvider(props: AuthProvider) {
 
     localStorage.setItem("@dowhile:token", token);
 
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+
     setUser(user);
   }
 
