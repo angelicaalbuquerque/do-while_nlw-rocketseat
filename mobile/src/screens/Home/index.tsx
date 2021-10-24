65cf3afff8181eb0a271db3947a24bd786398238
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-24 12:02:41 -0300

    Criação do componente SendMessageForm

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -4,6 +4,7 @@ import { View } from "react-native";
 import { Header } from "../../components/Header";
 import { MessageList } from "../../components/MessageList";
 import { SignInBox } from "../../components/SignInBox";
+import { SendMessageForm } from "../../components/SendMessageForm";
 
 import { styles } from "./styles";
 
@@ -12,7 +13,7 @@ export function Home() {
     <View style={styles.container}>
       <Header />
       <MessageList />
-      <SignInBox />
+      <SendMessageForm />
     </View>
   );
 }
