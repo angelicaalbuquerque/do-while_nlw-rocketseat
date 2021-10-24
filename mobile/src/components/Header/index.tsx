235994877c4e58840846cfb289989f35b54d5c09
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-24 00:00:06 -0300

    Conclusão do Header da aplicação

--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
+
 import { styles } from "./styles";
+
+import { UserPhoto } from "../UserPhoto";
 
 import LogoSvg from "../../assets/logo.svg";
 
@@ -8,9 +11,14 @@ export function Header() {
   return (
     <View style={styles.container}>
       <LogoSvg />
-      <TouchableOpacity>
-        <Text style={styles.logoutText}>Sair</Text>
-      </TouchableOpacity>
+
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+
+        <UserPhoto imageUri="https://github.com/angelicaalbuquerque.png" />
+      </View>
     </View>
   );
 }
