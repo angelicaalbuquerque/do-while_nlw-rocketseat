2de9d042e3eb039b2cf99b53d4dc82ed6a835d09
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-24 11:10:11 -0300

    Animação com Moti em lista de mensagens

--- mobile/src/components/Message/index.tsx
+++ mobile/src/components/Message/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View, Text } from "react-native";
+import { MotiView } from "moti";
 
 import { UserPhoto } from "../UserPhoto";
 
@@ -20,7 +21,12 @@ type Props = {
 
 export function Message({ data }: Props) {
   return (
-    <View style={styles.container}>
+    <MotiView
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: "timing", duration: 700 }}
+      style={styles.container}
+    >
       <Text style={styles.message}>{data.text}</Text>
 
       <View style={styles.footer}>
@@ -28,6 +34,6 @@ export function Message({ data }: Props) {
 
         <Text style={styles.userName}>{data.user.name}</Text>
       </View>
-    </View>
+    </MotiView>
   );
 }
