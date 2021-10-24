a08aae2d0556a09083f055cec22720a37dba9645
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-23 21:55:23 -0300

    Criação da interface Home

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Olááááááá!</Text>
+    </View>
+  );
+}
