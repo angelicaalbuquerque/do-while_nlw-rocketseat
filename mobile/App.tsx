d4df4082a81589ffb4bf53807b747d6570ca17de
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-23 22:17:28 -0300

    Fonte personalizada instalada e lógica de loading aplicada

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,22 @@
 import React from "react";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import Apploading from "expo-app-loading";
+
 import { Home } from "./src/screens/Home";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Apploading />;
+  }
+
   return <Home />;
 }
