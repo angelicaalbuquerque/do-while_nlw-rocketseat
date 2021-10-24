027faa58bb756f8858e11e9592a1157d1ba4a4c5
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-10-24 13:44:20 -0300

    Mensagens em Real Time

--- mobile/src/components/MessageList/index.tsx
+++ mobile/src/components/MessageList/index.tsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ScrollView } from "react-native";
+import { io } from "socket.io-client";
+// import { MESSAGES_EXAMPLE } from '../../utils/messages';
 
 import { styles } from "./styles";
 
-import { Message } from "../Message";
+import { Message, MessageProps } from "../Message";
+import { api } from "../../services/api";
+
+let messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+socket.on("new_message", (newMessage) => {
+  messagesQueue.push(newMessage);
+});
 
 export function MessageList() {
-  const message = {
-    id: "1",
-    text: "teste",
-    user: {
-      name: "angelica",
-      avatar_url: "https://github.com/angelicaalbuquerque.png",
-    },
-  };
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const messagesResponse = await api.get<MessageProps[]>("/messages/last3");
+      setCurrentMessages(messagesResponse.data);
+    }
+
+    fetchMessages();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setCurrentMessages((prevState) => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1],
+        ]);
+        messagesQueue.shift();
+      }
+    }, 3000);
+
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <ScrollView
@@ -21,9 +48,9 @@ export function MessageList() {
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps="never"
     >
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
+      {currentMessages.map((message) => (
+        <Message key={message.id} data={message} />
+      ))}
     </ScrollView>
   );
 }
